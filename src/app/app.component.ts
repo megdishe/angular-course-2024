036762557612cd4fde68a72bb32e7b72bb541842
megdishemyga <140427058+megdishemyga@users.noreply.github.com>
2024-09-20 19:54:53 +0200

    section 2: new @for(user of users; track user.id){....} @If() {...} syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
 import {NgForOf} from "@angular/common";
 import {DUMMY_USERS} from "./dummy-users";
+import {TaskComponent} from "./task/task.component";
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,18 @@ import {DUMMY_USERS} from "./dummy-users";
   imports: [
     HeaderComponent,
     UserComponent,
-    NgForOf
+    NgForOf,
+    TaskComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
+  selectedUsername?: string;
   protected readonly users = DUMMY_USERS;
-  onSelectedUser(id : string){
+
+  onSelectedUser(id: string) {
+    let selectedUser = this.users.find(value => value.id == id);
+    this.selectedUsername = selectedUser?.name;
   }
 }
